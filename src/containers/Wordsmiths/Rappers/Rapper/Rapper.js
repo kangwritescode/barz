335657b14ff884f9120e39db545655b30e9216cd
kangwritescode <kangwritescode@gmail.com>
2019-11-17 02:00:02 -0800

    region colors

--- src/containers/Wordsmiths/Rappers/Rapper/Rapper.js
+++ src/containers/Wordsmiths/Rappers/Rapper/Rapper.js
@@ -3,13 +3,20 @@ import { connect } from 'react-redux'
 import './Rapper.css'
 
 function Rapper(props) {
+    const colorDict = {
+        'West': 'yellow',
+        'South': 'turquoise',
+        'Midwest': 'darkred',
+        'East': 'green'
+
+    }
     
     return (
         <div className="rapper" onClick={() => props.shineSpotlight(props)} id={props.style}>
             <div className="rapperDetail rapper-detail-font" id="votes">{props.votes}</div>
-            <div className="rapperDetail rapper-detail-font" id="username">{props.username}</div>
+            <div className="rapperDetail rapper-detail-font" id="username"><b>{props.username}</b></div>
             <div className="rapperDetail rapper-detail-font" id="location">{`${props.city}, ${props.state}`}</div>
-            <div className="rapperDetail rapper-detail-font" id="coast">{props.coast}</div>
+            <div className="rapperDetail rapper-detail-font" id="coast"><span style={{color: colorDict[props.coast]}}>{props.coast}</span></div>
             <div className="rapperDetail rapper-detail-font" id="gender">{props.gender}</div>
         </div>
     )
